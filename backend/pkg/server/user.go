3ed24a61daca0daaabaca9aeae17cbf5ae433bb6
hsingyingli <xingyingli855@gmail.com>
2023-06-03 10:45:55 +0800

    feat(backend:server/user.go): add create user action

--- backend/pkg/server/user.go
+++ backend/pkg/server/user.go
@@ -2,6 +2,7 @@ package server
 
 import (
 	"net/http"
+	"time"
 
 	"github.com/gin-gonic/gin"
 	db "github.com/hsingyingli/yourslack-backend/db/sqlc"
@@ -12,6 +13,13 @@ type CreateUserRequest struct {
 	Username string `form:"username" json:"username" binding:"required"`
 	Email    string `form:"email" json:"email" binding:"required"`
 	Password string `form:"password" json:"password" binding:"required"`
+}
+
+type CreateUserResponse struct {
+	ID       int64     `json:"id"`
+	Username string    `json:"username"`
+	Email    string    `json:"email"`
+	UpdateAt time.Time `json:"updated_at"`
 }
 
 func (server *Server) createUser(ctx *gin.Context) {
@@ -39,5 +47,12 @@ func (server *Server) createUser(ctx *gin.Context) {
 		return
 	}
 
-	ctx.JSON(http.StatusOK, user)
+	rsp := CreateUserResponse{
+		ID:       user.ID,
+		Username: user.Username,
+		Email:    user.Email,
+		UpdateAt: user.UpdatedAt,
+	}
+
+	ctx.JSON(http.StatusOK, rsp)
 }
